50c40bf3c3619a7432e7de86df5d94bc4146a847
Peumariano <pedromariano747@gmail.com>
2023-04-06 23:02:51 -0300

    finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'
-import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { UserModule } from './user/user.module';
+import { MeetModule } from './meet/meet.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     AuthModule,
-    UserModule
+    UserModule,
+    MeetModule,
+    RoomModule
   ],
   controllers: [],
   providers: [
